806f0aa7ad62b82ba7808ed28669338ffb9973f4
Day <107324462+DayXL@users.noreply.github.com>
2022-12-05 19:43:43 -0300

    Adicionado reação

--- scripts/teste.js
+++ scripts/teste.js
@@ -3,3 +3,9 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Tá aqui!";
 
 alert("Seja bem vindo!");
+
+document.querySelector("html").addEventListener("click", function () {
+
+    alert("Você clicou aqui!");
+    
+});
