dde66e92c7b405718e15be6d75a5255d9c583509
Day <107324462+DayXL@users.noreply.github.com>
2022-12-05 19:46:04 -0300

    nova reação

--- scripts/teste.js
+++ scripts/teste.js
@@ -4,8 +4,20 @@ myHeading.textContent = "Tá aqui!";
 
 alert("Seja bem vindo!");
 
-document.querySelector("body").addEventListener("click", function () {
+document.querySelector("h1").addEventListener("click", function () {
 
-    alert("Você clicou aqui!");
+    alert("Você clicou no título principal");
+    
+});
+
+document.querySelector("h2").addEventListener("click", function () {
+
+    alert("Você clicou no subtítulo ");
+    
+});
+
+document.querySelector("h1").addEventListener("click", function () {
+
+    alert("Você clicou no subtítulo ");
     
 });
