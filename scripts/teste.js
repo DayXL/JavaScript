5c6e29de5fb0571b8d9a35182f75ad37f77706c4
Day <107324462+DayXL@users.noreply.github.com>
2022-12-05 20:45:40 -0300

    criado interação para bptão

--- scripts/teste.js
+++ scripts/teste.js
@@ -14,3 +14,32 @@ myImage.onclick = function() {
     }
 
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Qual seu nome?');
+    if(!myName) {
+        setUserName();
+    } 
+    
+    else {
+        localStorage.setItem('Nome', myName);
+        myHeading.innerHTML = 'Gatos são demais, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('Nome')) {
+  setUserName();
+} 
+
+else {
+  let storedName = localStorage.getItem('Nome');
+  myHeading.innerHTML = 'Gatos são demais, ' + storedName;
+
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
