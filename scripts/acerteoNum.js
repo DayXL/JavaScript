2853e383732e969bfa3db6cf3a5b225becafc2cb
Day <107324462+DayXL@users.noreply.github.com>
2022-12-05 21:22:01 -0300

    criada suposta função funcional

--- scripts/acerteoNum.js
+++ scripts/acerteoNum.js
@@ -3,3 +3,29 @@ function gerarNumero() {
     return Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+let mensagem = document.querySelector('p');
+
+myButton.onclick = function() {
+    let num = gerarNumero();
+    myHeading.innerHTML = 'Números sorteados:' + num;
+}
+
+function numeroEscolhido() {
+    let numEscolhido = document.querySelector('input');
+
+    if (numEscolhido === num) {
+        mensagem.innerHTML ('Parabéns você acertou!');
+    } 
+
+    else if (numEscolhido > num) {
+        mensagem.innerHTML ('O número sorteado é menor!');
+    } 
+
+    else {
+        mensagem.innerHTML ('O número sorteado é maior!');
+    }
+
+}
